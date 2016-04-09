4391ccafaa0d3485df399e818abe4bbf192cd606
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-04-09 13:57:57 -0400

    Working

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -1,11 +1,12 @@
 describe('Directive Test', function(){
   browser.get('http://localhost:8080');
 
-  var firstUser = element(by.css('.user'));
-  // console.log(firstUser);
-  browser.debugger();
+  var users = element.all(by.css('.user'));
+  var firstUser = users.get(0).getText();
+
   it('should have name filled in', function(){
-    expect(firstUser.children[1].textContent).toEqual("Name: Bill Gates");
+    expect(firstUser).toI
+    // expect(firstUser.children[1].textContent).toEqual("Name: Bill Gates");
   })
 
 });
