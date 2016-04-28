ffb48e93a3f84bde7c6f101bae270fb796ccd46c
Jacob <mailme711@gmail.com>
2016-04-28 13:44:58 -0400

    Done.

---
+++ spec/Index.spec.js
@@ -0,0 +1,16 @@
+describe('Directive Test', function() {
+	browser.get('http://localhost:8080');
+
+  it('should have a name', function(){
+    expect(element.all(by.css('user-profile')).get(0).getText()).toContain('Name');
+  })
+
+  it('should have a position', function(){
+    expect(element.all(by.css('user-profile')).get(0).getText()).toContain('Position');
+  })
+
+  it('should have a description', function(){
+    expect(element.all(by.css('user-profile')).get(0).getText()).toContain('Description');
+  })
+
+  });
