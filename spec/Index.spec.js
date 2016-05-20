851c36781bb15093a05e7a75d1f424c14e0b1299
jsgoldb <jgoldb@yahoo.com>
2016-05-20 09:28:28 -0500

    Done.

---
+++ spec/Index.spec.js
@@ -0,0 +1,8 @@
+describe('User Profile Directive test', function() {
+  browser.get('http://localhost:8080');
+  var name = element(by.css('.user:nth-child(0)'));
+  var position = element(by.css('.user:nth-child(1)'));
+  var description = element(by.css('.user:nth-child(2)'));
+  
+
+})
