652ec87170af3ce8238abfd49664169c58e0e8de
evanscloud <dijkstrascloud@gmail.com>
2016-05-30 22:35:14 -0400

    Test to make sure content exists and renders proper elements

---
+++ spec/Index.spec.js
@@ -0,0 +1,17 @@
+describe('Directive Test', function() {
+    browser.get('http://localhost:8080');
+
+    var userProfile = element.all(by.css('user-profile'));
+
+    it('should have a name', function () {
+        expect(userProfile.get(0).getText()).toContain('Name');
+    });
+
+    it('should have a position', function () {
+        expect(userProfile.get(0).getText()).toContain('Position');
+    });
+
+    it('should have a description', function () {
+        expect(userProfile.get(0).getText()).toContain('Description');
+    });
+});
