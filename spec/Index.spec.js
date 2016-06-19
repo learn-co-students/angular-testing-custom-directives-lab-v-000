57e0c72cc384a58afa951ec2c7a269028253388e
seanliu93 <sean93liu@gmail.com>
2016-06-19 19:00:47 -0400

    Done.

---
+++ spec/Index.spec.js
@@ -0,0 +1,17 @@
+describe('Index Test', function() {
+    browser.get('http://localhost:8080');
+
+    var userProfile = element.all(by.css('user-profile'));
+
+    it('check name', function () {
+        expect(userProfile.get(0).getText()).toContain('Name');
+    });
+
+    it('check position', function () {
+        expect(userProfile.get(0).getText()).toContain('Position');
+    });
+
+    it('check description', function () {
+        expect(userProfile.get(0).getText()).toContain('Description');
+    });
+});
