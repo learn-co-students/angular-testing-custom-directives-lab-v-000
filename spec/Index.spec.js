843f4aa722101eb30da59fd4c2795584972fbde9
github username <github email address>
2016-07-13 18:27:31 -0400

    Done.

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -6,15 +6,15 @@ describe('UserProfile Test', function() {
     var description = element(by.css('.user p'));
 
     it('should properly display name', function () {
-        expect(name.getInnerHtml()).toEqual('Name: Bill Gates');
+        expect(name.getInnerHtml()).toEqual('Bill Gates');
     });
 
     it('should properly display position', function () {
-        expect(position.getInnerHtml()).toEqual('Position: Chairman, Microsoft');
+        expect(position.getInnerHtml()).toEqual('Chairman, Microsoft');
     });
 
     it('should properly display position', function () {
-        expect(description.getInnerHtml()).toEqual('Description: William Henry "Bill" Gates III (born October 28, 1955) is an American business magnate, philanthropist, investor, and computer programmer. In 1975, Gates and Paul Allen co-founded Microsoft, which became the world\'s largest PC software company. During his career at Microsoft, Gates held the positions of chairman, CEO and chief software architect, and was the largest individual shareholder until May 2014. Gates has authored and co-authored several books.');
+        expect(description.getInnerHtml()).toEqual('William Henry "Bill" Gates III (born October 28, 1955) is an American business magnate, philanthropist, investor, and computer programmer. In 1975, Gates and Paul Allen co-founded Microsoft, which became the world\'s largest PC software company. During his career at Microsoft, Gates held the positions of chairman, CEO and chief software architect, and was the largest individual shareholder until May 2014. Gates has authored and co-authored several books.');
     });
 
 });
