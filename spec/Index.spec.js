e085ebb5d6b1a2915c2576c6bae680e00c0b0943
agdavid <antonio.g.david@gmail.com>
2016-09-02 16:11:29 -0400

    begin directive test, mock browser

---
+++ spec/Index.spec.js
@@ -0,0 +1,13 @@
+describe('UserProfile Directive Test', function() {
+  browser.get('http://localhost:8080');
+  
+});
+
+
+
+      // '<div class="user">',
+      //   '<h2>User Profile</h2>',
+      //   '<div>Name: <span ng-transclude="name"></span></div>',
+      //   '<div>Position: <span ng-transclude="position">No position</span></div>',
+      //   '<div>Description: <span ng-transclude="description">No description</span></div>',
+      // '</div>'
