745287c0aa46d677aaf738d9a0334370bd88bc67
agdavid <antonio.g.david@gmail.com>
2016-09-02 16:36:17 -0400

    add test for Name label and value

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -1,6 +1,20 @@
 describe('UserProfile Directive Test', function() {
   browser.get('http://localhost:8080');
+
+  var directive = element.all(by.css('user-profile'))
+  it('should render the directive', function() {
+    expect(directive.getInnerHtml()).not.toEqual('');
+  });
   
+  it('should contain the Name label', function() {
+    expect(directive.get(0).getText()).toContain('Name:');
+  });
+  it('should contain the right name', function() {
+    //expect(directive.get(0).getText()).toContain('Bill Gates');
+    expect(directive.get(0).element(by.css('h4')).getInnerHtml()).toContain('Bill Gates');
+  });
+
+
 });
 
 
