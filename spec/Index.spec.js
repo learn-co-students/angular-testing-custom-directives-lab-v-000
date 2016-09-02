7eae398df9f03af32027a064034a70544ba0dae2
agdavid <antonio.g.david@gmail.com>
2016-09-02 16:47:24 -0400

    Done.

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -2,18 +2,20 @@ describe('UserProfile Directive Test', function() {
   browser.get('http://localhost:8080');
 
   var directive = element.all(by.css('user-profile'))
+  //test for content, generally, in a directive
   it('should render the directive', function() {
     expect(directive.getInnerHtml()).not.toEqual('');
   });
   
+  //test for first directive (i.e., index 0) label and value
   it('should contain the "Name:" label', function() {
     expect(directive.get(0).getText()).toContain('Name:');
   });
   it('should contain the right name', function() {
-    //expect(directive.get(0).getText()).toContain('Bill Gates');
     expect(directive.get(0).element(by.css('h4')).getInnerHtml()).toContain('Bill Gates');
   });
 
+   //test for first directive (i.e., index 0) label and value
   it('should contain the "Position:" label', function() {
     expect(directive.get(0).getText()).toContain('Position');
   });
@@ -21,6 +23,7 @@ describe('UserProfile Directive Test', function() {
     expect(directive.get(0).element(by.css('h6')).getInnerHtml()).toContain('Chairman, Microsoft');
   });
 
+   //test for first directive (i.e., index 0) label and value
   it('should contain the "Description: " label', function() {
     expect(directive.get(0).getText()).toContain('Description:');
   });
@@ -28,14 +31,4 @@ describe('UserProfile Directive Test', function() {
     expect(directive.get(0).element(by.css('p')).getInnerHtml()).toContain('William Henry \"Bill\" Gates III \(born October 28, 1955\) is an American business magnate');
   });
 
-
 });
-
-
-
-      // '<div class="user">',
-      //   '<h2>User Profile</h2>',
-      //   '<div>Name: <span ng-transclude="name"></span></div>',
-      //   '<div>Position: <span ng-transclude="position">No position</span></div>',
-      //   '<div>Description: <span ng-transclude="description">No description</span></div>',
-      // '</div>'
