3d120f684afb25f9ede476bb2211b87d2b5cff23
sensei100 <jenanton01@gmail.com>
2016-10-26 10:36:58 -0400

    Done.

---
+++ spec/Index.spec.js
@@ -0,0 +1,21 @@
+describe('Directive Test', function() {
+  browser.get('http://localhost:8080');
+
+  var directive = element.all(by.css('user-profile'));
+
+  it('should correctly display a name', function() {
+    expect(directive.get(0).getText()).toContain('Name:');
+    expect(directive.get(1).getText()).toContain('Tim Cook')
+  });
+
+    it('should correctly display a position', function() {
+    expect(directive.get(0).getText()).toContain('Position:');
+    expect(directive.get(1).getText()).toContain('CEO')
+  });
+
+    it('should correctly display a description', function() {
+    expect(directive.get(0).getText()).toContain('Description:');
+  });
+
+
+})
